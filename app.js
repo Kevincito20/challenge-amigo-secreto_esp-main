fc85518ec6741090a267f1fd3979da6413f94b32
Kevin <kevin.linares1@utp.ac.pa>
2025-02-03 23:10:38 -0500

    Desarrollo del programa terminado

--- app.js
+++ app.js
@@ -3,26 +3,37 @@
 //Funcion para escribir el nombre de un participante
 function agregarAmigo(){
     let nombre = document.getElementById("amigo").value;
-    validarEntrada(nombre);
+    if(!validarEntrada(nombre)){
+        return;
+    }
     let lista = document.getElementById("listaAmigos");
     let elemento = document.createElement("li");
     elemento.textContent = nombre;
     lista.appendChild(elemento);
-    console.log(nombre);
     document.getElementById("amigo").value = "";
 }
 
+//validacion de entrada vacía y solo letras
 function validarEntrada(nombre){
+    let letrasPermitidas = /^[a-zA-Z]+$/;
+
     if(nombre === ""){
         alert("Debe ingresar un nombre");
-        return;
+        return false;
     }
+
+    if(!nombre.match(letrasPermitidas)){
+        alert("El nombre debe contener solo letras");
+        return false;    
+    }
+
+    return true;
 }
 
+//Funcion para sortear un amigo de la lista
 function sortearAmigo(){
     let lista = document.getElementById("listaAmigos");
     let amigo = lista.children[Math.floor(Math.random() * lista.children.length)];
     let nombreAmigo = amigo.textContent;
-    document.getElementById("resultado").textContent = nombreAmigo;
-    console.log(nombreAmigo);
+    document.getElementById("resultado").textContent = "Tu amigo secreto es: " + nombreAmigo;
 }
